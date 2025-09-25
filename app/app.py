cc132c965a5dca0abce932b324bf16dbee7c5e7a
David Riley-CYP <driley2@jhg.com.au>
2025-09-25 12:13:01 +1000

    Update to DB

--- app/app.py
+++ app/app.py
@@ -9,7 +9,6 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [
     dbc.themes.BOOTSTRAP,
     "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
-    "assets/styles.css"
 ]
 
 app = dash.Dash(
